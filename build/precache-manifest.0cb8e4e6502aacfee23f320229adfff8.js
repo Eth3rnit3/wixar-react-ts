4a69cb4ad0f5060087a6bc5685b9ce1fddae0a41
David <eth3rnit3@outlook.fr>
2020-05-21 09:37:14 +0200

    build app

---
+++ build/precache-manifest.0cb8e4e6502aacfee23f320229adfff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307a108d19a574478fac76a01a347609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5ebbca41b841b772974",
+    "url": "/static/css/main.5e296bff.chunk.css"
+  },
+  {
+    "revision": "392b3801223998833698",
+    "url": "/static/js/2.3d40c7ca.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/static/js/2.3d40c7ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ebbca41b841b772974",
+    "url": "/static/js/main.822f9bc7.chunk.js"
+  },
+  {
+    "revision": "2df6cfef353483e6c9b0",
+    "url": "/static/js/runtime-main.ff441184.js"
+  }
+]);
